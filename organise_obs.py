3fbe0188b1262c3fcaac2d4fec96448210ce7c2d
Adam Rains <adam.d.rains@gmail.com>
2018-05-03 14:21:25 +1000

    Restructured organise_obs.py to account for dealing many files per target

--- organise_obs.py
+++ organise_obs.py
@@ -142,8 +142,27 @@ seq_label = ["bright", "faint"]
 # - X: aborted
 # - ? or "-": error, will be repeated
 # Given this, A and B observations will be accepted
+def check_and_save_good_sequence(complete_sequences, sequence, concatentation):
+    """
+    """
+    # Only add if all observations in sequence are good
+    grade = (night_log[night][ob_i][3] + night_log[night][ob_i+1][3]
+             + night_log[night][ob_i+2][3] + night_log[night][ob_i+3][3]
+             + night_log[night][ob_i+4][3])
+         
+    # Determine start and end times of the concatenation
+    start = night_log[night][ob_i][4]
+    end = night_log[night][ob+4][4]
+    
+    # If all obs have acceptable grades the sequence exists
+    if ("C" not in grade and "D" not in grade and "-" not in grade 
+        and "?" not in grade and "X" not in grade):
+        complete_sequences.append([concatentation, seq_label[seq_i], night, start, end, 
+                                   grade])  
+    
 
-complete_sequences = []
+
+complete_sequences = {}
 
 # For every night of observations...
 for night in night_log.keys():
@@ -151,13 +170,82 @@ for night in night_log.keys():
     for seq_i, sequence in enumerate(all_sequences):
         # For every science target...
         for sci in sequence:
+            # Step through the nightly observations attempting to match
+            # the CAL-SCI-CAL-SCI-CAL sequences where each CAL or SCI has many
+            # different files (N exposures, darks, kappa matrices)
+            # Do this by building up the list of observations until you either
+            # reach the end of the final calibrator (i.e. complete sequence) or 
+            # something else happens (i.e. broken sequence)
+            ob_i = 0            # The ith observation that night
+            tgt_i = 0           # The ith target in the CAL-SCI sequence
+            concatenation = []  # Current list of obs from CAL-SCI sequence
+            
             # For every observation in the night...
-            for ob_i, observation in enumerate(night_log[night]):
+            while ob_i < len(night_log[night]):
+                # Adding entry for current target, increment ob_i
+                if sequence[sci][tgt_i] in night[ob_i][2]:
+                    concatenation.append(night[ob_i])
+                    ob_i += 1
+                
+                # Adding entry for next target, increment ob_i and tgt_i
+                elif ((tgt_i + 1 < len(sequence[sci])) 
+                    and sequence[sci][tgt_i+1] in night[ob_i][2]):
+                    concatenation.append(night[ob_i])
+                    ob_i += 1
+                    tgt_i += 1
+                    
+                # Completed sequence, increment ob_i and reset tgt_i
+                elif ((tgt_i + 1 == len(sequence[sci]))         # Last tgt &&
+                    and (ob_i + 1 == len(night)                 # Last ob OR
+                    or (sequence[sci][tgt_i] not in night[ob_i][2]))):    # Non-seq ob
+                    
+                    all_grades = [concatenation[i][3] for i in concatenation]
+                    
+                    # If all obs have acceptable grades the sequence exists
+                    if ("C" not in grade and "D" not in grade 
+                        and "-" not in grade and "?" not in grade 
+                        and "X" not in grade):
+                        complete_sequences[(sci, seq_label[seq_i], grade)] = concatentation
+                        #complete_sequences.append([concatentation, seq_label[seq_i], night, start, end, 
+                                   #grade])  
+                
+                # Broken sequence, discard concatenation, reset tgt_i
+                else:
+                    tgt_t = 0
+                    concatenation = []
+            
+                """
+                # If observation is the current (tgt_i) target, add to 
+                # concatenation and increment observation counter
+                if sci[tgt_i] in night[ob_i][2]:
+                    concatenation.append(night[ob_i])
+                    ob_i += 1
+                    
+                # If observation is *next* target in concatenation, add to seq
+                # and increment both the target and observation counters
+                elif tgt_i + 1 < len(sci) and sci[tgt_i+1] in night[ob_i][2]:
+                    concatenation.append(night[ob_i])
+                    ob_i += 1
+                    tgt_i += 1
+                    
+                # If observation is not in sequence, but last observation was
+                # of the last calibrator in the sequence, consider the seq
+                # complete and save
+                elif tgt_i + 1 == len(sci) and sci[tgt_i] not in night[ob_i][2]:
+                    pass
+                
+                # If not current target, and not at end of sequence, consider
+                # incomplete and discard current progress and reset (but don't
+                # reset ob counter, as we want to test this ob again from seq 
+                # start)
+                else:
+                    tgt_i = 0
+                    concatenation = []
+                
+                
+                
                 try:
-		    if (sequence[sci][0] in night_log[night][ob_i][2]
-			and sequence[sci][1] in night_log[night][ob_i+1][2]
-			and sequence[sci][2] in night_log[night][ob_i+2][2]):
-			print sci
+
                     # Determine if the correct sequence exists in order
                     if (sequence[sci][0] in night_log[night][ob_i][2] 
                         and sequence[sci][1] in night_log[night][ob_i+1][2]
@@ -165,33 +253,14 @@ for night in night_log.keys():
                         and sequence[sci][3] in night_log[night][ob_i+3][2]
                         and sequence[sci][4] in night_log[night][ob_i+4][2]):
                     	print "good seuqence"
-                        # Only add if all observations in sequence are good
-                        grade = (night_log[night][ob_i][3]
-                                 + night_log[night][ob_i+1][3]
-                                 + night_log[night][ob_i+2][3]
-                                 + night_log[night][ob_i+3][3]
-                                 + night_log[night][ob_i+4][3])
-                             
-                        # Determine start and end times of the concatenation
-                        start = night_log[night][ob_i][4]
-			end = night_log[night][ob+4][4]
-			#start = night_log[night][ob_i][3].split(" ")[0]
-                        #end = night_log[night][ob_i+4][3].split(" ")[-1]
                         
-                        # If all obs have acceptable grades the sequence exists
-                        if ("C" not in grade and "D" not in grade 
-                            and "-" not in grade and "?" not in grade
-                            and "X" not in grade):
-                            complete_sequences.append([sci, seq_label[seq_i], 
-                                                       night, start, end, 
-                                                       grade])  
                 except:
                     # We've reached the end of the night, can't fit a full 
                     # concatenation in
                     break
-
+                """
 # Done
-complete_sequences.sort()
+#complete_sequences.sort()
 
 # -----------------------------------------------------------------------------
 # Copy the completed sequences to a new directory structure
