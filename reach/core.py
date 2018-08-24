cc25d85e281873b2364240fe00d5b00b77a0f1bb
Adam Rains <adam.d.rains@gmail.com>
2018-08-24 12:11:33 +1000

    Split Kervella and Boyajian LDD relations into different functions, Boyajian relation now predicts LDD (without uncertainty)

--- reach/core.py
+++ reach/core.py
@@ -6,48 +6,78 @@ import numpy as np
 import matplotlib.pylab as plt
 from scipy.interpolate import interp1d
 
-# Currently most functions will be placeholders until requirements are sorted
-
-class UnknownRelationException(Exception):
-    pass
-
 class ColourOutOfBoundsException(Exception):
     pass
 
-def calculate_vk_diameter(v_mag, v_mag_err, k_mag, k_mag_err, 
-                          relation="kervella2004"):
-    """Calculate the V-K angular as predicted by colour-diameter relations from
-    the literature:
-    
-    1) Kervella et al. 2004:
-        http://adsabs.harvard.edu/abs/2004A%26A...426..297K
-        
-    2) Boyajian et al. 2014:
+def predict_ldd_boyajian(F1_mag, F1_mag_err, F2_mag, F2_mag_err, 
+                         colour_rel="V-W3"):
+    """Calculate the limb darkened angular diameter as predicted by 
+    colour-diameter relations from Boyajian et al. 2014:
         http://adsabs.harvard.edu/abs/2014AJ....147...47B
+        
+    Parameters
+    ----------
+    F1_mag: float
+        Magnitude of the star in the first filter.
+    F1_mag_err: float
+        Error in magnitude of the star in the first filter.
+    F2_mag: float
+        Magnitude of the star in the second filter.
+    F2_mag_err: float
+        Error in magnitude of the star in the second filter.
+    colour_rel: string
+        The colour relation to use for predicting the LDD (i.e. V-K)
+        
+    Returns
+    -------
+    ldd: float
+        The predicted limb darkened angular diameter
     """
-    # List of accepted literature relations
-    relations = ["kervella2004", "boyajian2014"]
+    # Import the Boyajian relations, columns are:
+    # [Colour Index, Num Points, Range (mag), a0, a0_err, a1, a1_err,
+    #  a2, a2_err, a3, a3_err, Reduced chi^2, err (%)]
+    boyajian_2014_rel_file = "boyajian_2014_colour_diameter_relations.csv"
     
-    if relation == "kervella2004":
-        # Calculate the LDD
-        log_ldd = 0.0755 * (v_mag - k_mag) + 0.5170 - 0.2 * k_mag
-        
-        ldd = 10**log_ldd
-        
-        # Calculate the error on this value (assuming no covariance)
-        log_ldd_err = np.sqrt((0.0755*v_mag_err)**2 + (0.2755*k_mag_err)**2)
-        
-        ldd_err = 10**(np.log(10)*log_ldd_err)
-        
-    elif relation == "boyajian":
-        pass
-        
-    else:
-        print("Unknown relation")
-        raise UnknownRelationException("Angular diameter relation must be in" 
-                                       "%s" % relations)
+    diam_rel = np.loadtxt(boyajian_2014_rel_file, delimiter=",", 
+                          skiprows=1, dtype="str")
+    # Create dictionary for coefficients
+    diam_rel_coeff = {}
+    
+    for rel in diam_rel:
+        diam_rel_coeff[rel[0]] = rel[3:11].astype(float)
+    
+    # Calculate diameters
+    # Relationship is log_diam = Sigma(i=0) a_i * (F1_mag-F2_mag) ** i
+    exponents = np.arange(0,4)
+    colour = F1_mag - F2_mag
+    
+    log_diam = np.sum(diam_rel_coeff[colour_rel][::2]*colour**exponents)
+    
+    ldd = 10**(-0.2*F1_mag) * 10**log_diam
+    
+    # Calculate the error (TODO)
+    ldd_err = 0
+    
+    return ldd, ldd_err
+    
+    
+def predict_ldd_kervella(V_mag, V_mag_err, K_mag, K_mag_err):
+    """Calculate the limb darkened angular diameter as predicted by 
+    colour-diameter relations from Kervella et al. 2004:
+        http://adsabs.harvard.edu/abs/2004A%26A...426..297K
+    """
+    # Calculate the LDD
+    log_ldd = 0.0755 * (V_mag - K_mag) + 0.5170 - 0.2 * K_mag
+
+    ldd = 10**log_ldd
+
+    # Calculate the error on this value (assuming no covariance)
+    log_ldd_err = np.sqrt((0.0755*V_mag_err)**2 + (0.2755*K_mag_err)**2)
+
+    ldd_err = 10**(np.log(10)*log_ldd_err)
                                        
-    return log_ldd, log_ldd_err, ldd, ldd_err
+    return log_ldd, log_ldd_err, ldd, ldd_err   
+     
 
 def calculate_ldd():
     """Calculates limb-darkened disk model to fit to observations as outlined 
